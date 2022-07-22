952fc28b108912a39b42cfeb007ba335131ee5a8
TheProgrammingBox <justinboxgea@gmail.com>
2022-07-22 16:17:27 -0700

    ya

--- TQueue.h
+++ TQueue.h
@@ -1,14 +1,17 @@
 #pragma once
 #include <memory>
 #include <iostream>
+#include <string>
+
+using namespace std;
 
 template <class T>
 class TQueue
 {
 private:
-    std::unique_ptr<T> head;
+    unique_ptr<T[]> head;
     int maxSize;
-    int size;
+    int currentSize;
 
 public:
     TQueue(int maxSize);
@@ -16,131 +19,292 @@ public:
     TQueue(TQueue &&source) noexcept;
     ~TQueue();
 
-    // assignment operators
     TQueue &operator=(TQueue &source);
     TQueue &operator=(TQueue &&source) noexcept;
 
-    // void enqueue(T data);
-    // T dequeue();
-
-    // bool isEmpty() const;
-    // bool isFull() const;
-    // int countSize() const;
-
-    // T front() const;
+    void enqueue(T value);
+    T dequeue();
+    T front() const;
+    int size() const;
+    bool isEmpty() const;
+    bool isFull() const;
+    void printQueue() const;
 };
 
+/*************************************************************************
+* Method TQueue: Class TQueue
+*------------------------------------------------------------------------
+* This function is the default constructor
+*------------------------------------------------------------------------
+* PRE-CONDITIONS
+* 		maxSize: the max size of the array
+
+* POST-CONDITIONS
+* 		initialized: maxSize, currentSize, head
+*************************************************************************/
 template <class T>
-TQueue<T>::TQueue(int maxSize) : maxSize(maxSize), size(0), head(std::make_unique<T>(maxSize))
+TQueue<T>::TQueue(int maxSize) : maxSize(maxSize), currentSize(0), head(make_unique<T[]>(maxSize))
 {
 }
 
+/*************************************************************************
+* Method TQueue: Class TQueue
+*------------------------------------------------------------------------
+* This function is the copy constructor
+*------------------------------------------------------------------------
+* PRE-CONDITIONS
+* 		&source: the object that will be copy
+
+* POST-CONDITIONS
+* 		will copy the object passed in into new object
+*************************************************************************/
 template <class T>
-TQueue<T>::TQueue(TQueue &source) : maxSize(source.maxSize), size(source.size), head(std::make_unique<T>(source.maxSize))
+TQueue<T>::TQueue(TQueue &source) : maxSize(source.maxSize), currentSize(source.currentSize), head(make_unique<T[]>(source.maxSize))
 {
-    for (int i = 0; i < source.size; i++)
+    for (int i = 0; i < source.currentSize; i++)
     {
         head[i] = source.head[i];
     }
 }
 
+/*************************************************************************
+* Method TQueue: Class TQueue
+*------------------------------------------------------------------------
+* This function is the move constructor
+*------------------------------------------------------------------------
+* PRE-CONDITIONS
+* 		&source: the object that will be move
+*
+* POST-CONDITIONS
+* 		will move the object passed in into new object
+*************************************************************************/
 template <class T>
-TQueue<T>::TQueue(TQueue &&source) noexcept : maxSize(source.maxSize), size(source.size), head(std::move(source.head))
+TQueue<T>::TQueue(TQueue &&source) noexcept : maxSize(source.maxSize), currentSize(source.currentSize), head(move(source.head))
 {
     source.maxSize = 0;
-    source.size = 0;
+    source.currentSize = 0;
     source.head = nullptr;
 }
 
+/*************************************************************************
+* Method ~TQueue: Class TQueue
+*------------------------------------------------------------------------
+* This function is the destructor
+*------------------------------------------------------------------------
+* PRE-CONDITIONS
+* 		none
+*
+* POST-CONDITIONS
+* 		will delete the object
+*************************************************************************/
 template <class T>
 TQueue<T>::~TQueue()
 {
 }
 
+/*************************************************************************
+* Method operator= copy: Class TQueue
+*------------------------------------------------------------------------
+* This function overload the assignment operator
+*------------------------------------------------------------------------
+* PRE-CONDITIONS
+* 		TQueue &source: the object being copy
+*
+* POST-CONDITIONS
+* 		*this: the new object after copy
+*************************************************************************/
 template <class T>
-TQueue<T> &TQueue<T>::operator=(TQueue &source) : maxSize(source.maxSize), size(source.size), head(std::make_unique<T>(source.maxSize))
+TQueue<T> &TQueue<T>::operator=(TQueue &source)
 {
-    for (int i = 0; i < source.size; i++)
+    if (this != &source)
     {
-        head[i] = source.head[i];
+        maxSize = source.maxSize;
+        currentSize = source.currentSize;
+        head = make_unique<T[]>(source.maxSize);
+        for (int i = 0; i < source.currentSize; i++)
+        {
+            head[i] = source.head[i];
+        }
     }
     return *this;
 }
 
+/*************************************************************************
+* Method operator= move: Class TQueue
+*------------------------------------------------------------------------
+* This function overload the assignment operator
+*------------------------------------------------------------------------
+* PRE-CONDITIONS
+* 		TQueue &source: the object being move
+*
+* POST-CONDITIONS
+* 		*this: the new object after move
+*************************************************************************/
 template <class T>
-TQueue<T> &TQueue<T>::operator=(TQueue &&source) noexcept : maxSize(source.maxSize), size(source.size), head(std::move(source.head))
+TQueue<T> &TQueue<T>::operator=(TQueue &&source) noexcept
 {
-    source.maxSize = 0;
-    source.size = 0;
-    source.head = nullptr;
+    if (this != &source)
+    {
+        maxSize = source.maxSize;
+        currentSize = source.currentSize;
+        head = move(source.head);
+        source.maxSize = 0;
+        source.currentSize = 0;
+        source.head = nullptr;
+    }
     return *this;
 }
 
-// template <class T>
-// void TQueue<T>::enqueue(T data)
-// {
-//     if (isFull())
-//     {
-//         std::cout << "Queue is full" << std::endl;
-//         return;
-//     }
-//     *tail = data;
-//     size++;
-//     tail++;
-// }
+/*************************************************************************
+* Method enqueue: Class TQueue
+*------------------------------------------------------------------------
+* This function will add a value to the end of the queue
+*------------------------------------------------------------------------
+* PRE-CONDITIONS
+* 		value: the value to be added
+*
+* POST-CONDITIONS
+* 		will add the value to the end of the queue
+*************************************************************************/
+template <class T>
+void TQueue<T>::enqueue(T value)
+{
+    if (currentSize == maxSize)
+    {
+        cout << "Queue is full" << endl;
+    }
+    else
+    {
+        head[currentSize] = value;
+        currentSize++;
+    }
+}
 
-// template <class T>
-// T TQueue<T>::dequeue()
-// {
-//     if (isEmpty())
-//     {
-//         std::cout << "Queue is empty" << std::endl;
-//         return 0;
-//     }
-//     T data = *head;
-//     for (int i = 0; i < size - 1; i++)
-//     {
-//         head[i] = head[i + 1];
-//     }
-//     tail--;
-//     size--;
-//     return data;
-// }
+/*************************************************************************
+* Method dequeue: Class TQueue
+*------------------------------------------------------------------------
+* This function will remove a value from the front of the queue
+*------------------------------------------------------------------------
+* PRE-CONDITIONS
+*       none
+*
+* POST-CONDITIONS
+*       will remove the value from the front of the queue
+*************************************************************************/
+template <class T>
+T TQueue<T>::dequeue()
+{
+    if (currentSize == 0)
+    {
+        cout << "Queue is empty" << endl;
+        return 0;
+    }
+    else
+    {
+        T value = head[0];
+        for (int i = 0; i < currentSize - 1; i++)
+        {
+            head[i] = head[i + 1];
+        }
+        currentSize--;
+        return value;
+    }
+}
 
-// template <class T>
-// bool TQueue<T>::isEmpty() const
-// {
-//     return size == 0;
-// }
 
-// template <class T>
-// bool TQueue<T>::isFull() const
-// {
-//     return size == maxSize;
-// }
+/*************************************************************************
+* Method front: Class TQueue
+*------------------------------------------------------------------------
+* This function will return the value at the front of the queue
+*------------------------------------------------------------------------
+* PRE-CONDITIONS
+*       none
+*
+* POST-CONDITIONS
+*       will return the value at the front of the queue
+*************************************************************************/
+template <class T>
+T TQueue<T>::front() const
+{
+    if (currentSize == 0)
+    {
+        cout << "Queue is empty" << endl;
+        return 0;
+    }
+    else
+    {
+        return head[0];
+    }
+}
 
-// template <class T>
-// int TQueue<T>::countSize() const
-// {
-//     return size;
-// }
+/*************************************************************************
+* Method size: Class TQueue
+*------------------------------------------------------------------------
+* This function will return the current size of the queue
+*------------------------------------------------------------------------
+* PRE-CONDITIONS
+*       none
+*
+* POST-CONDITIONS
+*       will return the size of the queue
+*************************************************************************/
+template <class T>
+int TQueue<T>::size() const
+{
+    return currentSize;
+}
 
-// template <class T>
-// T TQueue<T>::front() const
-// {
-//     return *head;
-// }
+/*************************************************************************
+* Method isEmpty: Class TQueue
+*------------------------------------------------------------------------
+* This function will return true if the queue is empty
+*------------------------------------------------------------------------
+* PRE-CONDITIONS
+*       none
+*
+* POST-CONDITIONS
+*       will return true if the queue is empty
+*************************************************************************/
+template <class T>
+bool TQueue<T>::isEmpty() const
+{
+    return currentSize == 0;
+}
 
-// template <class T>
-// TQueue<T> &TQueue<T>::operator=(const T &source)
-// {
-//     if (isFull())
-//     {
-//         std::cout << "Queue is full" << std::endl;
-//         return *this;
-//     }
-//     *tail = source;
-//     size++;
-//     tail++;
-//     return *this;
-// }
+/*************************************************************************
+* Method isFull: Class TQueue
+*------------------------------------------------------------------------
+* This function will return true if the queue is full
+*------------------------------------------------------------------------
+* PRE-CONDITIONS
+*       none
+*
+* POST-CONDITIONS
+*       will return true if the queue is full
+*************************************************************************/
+template <class T>
+bool TQueue<T>::isFull() const
+{
+    return currentSize == maxSize;
+}
+
+/*************************************************************************
+* Method printQueue: Class TQueue
+*------------------------------------------------------------------------
+* This function will print the queue
+*------------------------------------------------------------------------
+* PRE-CONDITIONS
+*       none
+*
+* POST-CONDITIONS
+*       will print the queue
+*************************************************************************/
+template <class T>
+void TQueue<T>::printQueue() const
+{
+    for (int i = 0; i < currentSize; i++)
+    {
+        cout << head[i] << " ";
+    }
+    cout << endl;
+}
--- hw12.cpp
+++ hw12.cpp
@@ -3,6 +3,111 @@
 int main()
 {
     std::cout << "Hello, World!!!" << std::endl;
-    TQueue<int> queue(100);
+    TQueue<int> intQueue(10);
+    TQueue<double>doubleQueue(10);
+    TQueue<string>stringQueue(10);
+
+    for (int i = 0; i < 6; i++)
+    {
+        intQueue.enqueue(i);
+        intQueue.printQueue();
+    }
+    
+    for (int i = 0; i < 4; i++)
+    {
+        intQueue.dequeue();
+        intQueue.printQueue();
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        intQueue.enqueue(i);
+        intQueue.printQueue();
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        intQueue.dequeue();
+        intQueue.printQueue();
+    }
+
+    for (int i = 0; i < 6; i++)
+    {
+        doubleQueue.enqueue(i / 2.0);
+        doubleQueue.printQueue();
+    }
+    
+    for (int i = 0; i < 4; i++)
+    {
+        doubleQueue.dequeue();
+        doubleQueue.printQueue();
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        doubleQueue.enqueue(i / 2.0);
+        doubleQueue.printQueue();
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        doubleQueue.dequeue();
+        doubleQueue.printQueue();
+    }
+
+    for (int i = 0; i < 6; i++)
+    {
+        stringQueue.enqueue("item");
+        stringQueue.printQueue();
+    }
+
+    for (int i = 0; i < 4; i++)
+    {
+        stringQueue.dequeue();
+        stringQueue.printQueue();
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        stringQueue.enqueue("item");
+        stringQueue.printQueue();
+    }
+    
+    for (int i = 0; i < 3; i++)
+    {
+        stringQueue.dequeue();
+        stringQueue.printQueue();
+    }
+    
+    while (!intQueue.isEmpty())
+    {
+        intQueue.dequeue();
+    }
+    if (intQueue.isEmpty())
+    {
+        std::cout << "Queue is empty" << std::endl;
+        intQueue.dequeue();
+    }
+    else
+    {
+        std::cout << "Queue is not empty" << std::endl;
+    }
+
+    while (!intQueue.isFull())
+    {
+        intQueue.enqueue(1);
+    }
+    if (intQueue.isFull())
+    {
+        std::cout << "Queue is full" << std::endl;
+        intQueue.enqueue(1);
+    }
+    else
+    {
+        std::cout << "Queue is not full" << std::endl;
+    }
+    
+    cout << "Front of queue: " << intQueue.front() << endl;
+
     return 0;
 }
